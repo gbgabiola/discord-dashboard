1697cd078dc96f385a47aa8e880cdfc4e8fb04e9
Genesis Gabiola <gbgabiola@gmail.com>
2021-11-19 00:59:35 +0800

    Setup database with typeorm and env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: Number.parseInt(process.env.MYSQL_DB_PORT),
+      username: process.env.MYSQL_DB_USER,
+      password: process.env.MYSQL_DB_PASS,
+      database: process.env.MYSQL_DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
