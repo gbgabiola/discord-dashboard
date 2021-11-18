0e7dd9373775a250d6443af40bda163a3d750cfa
Genesis Gabiola <gbgabiola@gmail.com>
2021-11-18 23:16:52 +0800

    Generate user and post service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { PostsController } from './controllers/posts/posts.controller';
+import { UserService } from './services/user/user.service';
+import { PostService } from './services/post/post.service';
 
 @Module({
   controllers: [UsersController, PostsController],
+  providers: [UserService, PostService],
 })
 export class UsersModule {}
