17913f133d08d352e4442ed8c6f4d3bd4be4e392
Genesis Gabiola <gbgabiola@gmail.com>
2021-11-19 13:19:28 +0800

    Setup authentication endpoints

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  /**
+   * GET /api/auth/login
+   * Route the user will visit to authenticate
+   */
+  @Get('login')
+  login() {
+    return;
+  }
+
+  /**
+   * GET /api/auth/redirect
+   * Redirect URL the OAuth2 provider will call
+   */
+  @Get('redirect')
+  redirect(@Res() res: Response) {
+    res.send(200);
+  }
+
+  /**
+   * GET /api/auth/status
+   * Retrieve the auth status
+   */
+  @Get('status')
+  status() {}
+
+  /**
+   * GET /api/auth/logout
+   * Log the user out to destroy the session
+   */
+  @Get('logout')
+  logout() {}
+}
